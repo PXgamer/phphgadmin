de39052b4552733661e581df0600a065c7656d29
PXgamer <owzie123@gmail.com>
2017-01-27 08:52:12 +0000

    A few minor updates listed below
    - Updated to force the `realpath` when adding to the ini
    - Reordered and redesigned the `repositories/repo_browse` view to not use tables

--- application/views/repositories/repo_browse.php
+++ application/views/repositories/repo_browse.php
@@ -1,33 +1,15 @@
-<table class="bigtable" style="border-bottom: 0">
-    <form action="<?php echo current_url(); ?>" method="post" id="form_hgrc" name="form_hgrc">
-        <input type="hidden" name="form_action" id="form_action" value=""/>
-        <tr>
-            <td colspan="3" align="right">
-                <p>
-                    <a href="<?php echo site_url('hgrepo/manage/'.the_name()); ?>"
-                       class="ui-state-default ui-corner-all dialog_link"><span
-                                class="ui-icon"></span><?php echo lang('hgphp_action_update_config'); ?></a>
-                </p>
-            </td>
-        </tr>
-        <tr>
-            <td colspan="3">
-                <div style="height:500px">
-                    <iframe src="<?php echo hgserve_url(the_name()); ?>" frameborder="0" border="0" cellspacing="0"
-                            style="display:block; width: 100%; height: 100%;">
-                        Your browser must support iframes to use this feature.
-                    </iframe>
-                </div>
-            </td>
-        </tr>
-        <tr>
-            <td colspan="3" align="right">
-                <p>
-                    <a href="<?php echo site_url('hgrepo/manage/'.the_name()); ?>"
-                       class="ui-state-default ui-corner-all dialog_link"><span
-                                class="ui-icon"></span><?php echo lang('hgphp_action_update_config'); ?></a>
-                </p>
-            </td>
-        </tr>
-</table>
-
+<form action="<?= current_url(); ?>" method="post" id="form_hgrc" name="form_hgrc">
+    <input type="hidden" name="form_action" id="form_action" value=""/>
+    <div class="form-group">
+        <iframe src="<?= $this->config->item('profile')['default']['hgserve_url'] . the_name(); ?>"
+                frameborder="0" border="0" cellspacing="0"
+                style="display:block; width: 100%; height: 100%;">
+            Your browser must support iframes to use this feature.
+        </iframe>
+    </div>
+    <div class="form-group">
+        <a href="<?= site_url('hgrepo/manage/' . the_name()); ?>" class="btn btn-default btn-xs pull-right">
+            <span class="fa fa-fw fa-cog"></span> Update Config
+        </a>
+    </div>
+</form>
